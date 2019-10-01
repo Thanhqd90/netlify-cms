5b4a59f7052eb0902bb542619a08624e2a5a4981
Thanh Dang <thanhqd1990@gmail.com>
2019-09-30 20:49:29 -0500

    First commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -13,8 +12,8 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
-            alt="Kaldi"
+            src={'https://logoipsum.com/assets/logo/logo-3.svg'}
+            alt="Logo"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+// import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={'https://logoipsum.com/assets/logo/logo-3.svg'} alt="Logo" style={{ width: '100px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -59,7 +58,7 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
+            {/* <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
               </Link>
@@ -75,18 +74,23 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
-            </div>
+            </div> */}
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+            <Link className="navbar-item" to="/about">
+                Services
+              </Link>
+              <Link className="navbar-item" to="/projects">
+                Projects
+              </Link>
+              {/* <Link className="navbar-item" to="/blog">
+                Blog
+              </Link> */}
+              <Link className="navbar-item" to="/contact">
+                Contact
+              </Link>
+              {/* <Link className="navbar-item" to="/contact/examples">
+                Form Examples
+              </Link> */}
             </div>
           </div>
         </div>
